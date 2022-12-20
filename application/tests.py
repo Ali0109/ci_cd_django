1f2439285171de7b97e69fba5c228f352c3b40e7
Alibek <kidiraliev97@gmail.com>
2022-12-20 10:44:50 +0500

    add test coverage and flake8

--- application/tests.py
+++ application/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.application = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        d = self.application
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
